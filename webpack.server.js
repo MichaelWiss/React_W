e153aef1fe92af2b96a03ce5016f3f508dc9bbef
MichaelWiss <michael.wiss@gmail.com>
2018-02-24 13:46:43 -0600

    webpack

---
+++ webpack.server.js
@@ -0,0 +1,10 @@
+module.exports = {
+   //inform webpack that we are building a bundle for node js
+   target: 'node',
+   //tell webpack the root file of our server app
+   entry: './src/index.js',
+   //tell webpack where to put the output file
+   output: {
+
+   }
+};
