4ef1cca217dabcc767a0fa35b5faad6a1475d79e
MichaelWiss <michael.wiss@gmail.com>
2018-02-24 14:01:27 -0600

    regex server

--- webpack.server.js
+++ webpack.server.js
@@ -16,7 +16,16 @@ module.exports = {
    	rules: [
    	{
       test: /\.js?$/,
-   	}
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+      	presets: [
+      	'react',
+      	'stage-0',
+        ['env', { targets: { browsers: ['last 2 versions'] }}] 
+      	]
+      }
+   	 }
    	]
    }
 };
