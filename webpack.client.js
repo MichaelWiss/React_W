1b3bd5d00396065fa447eeb270bdd1bba130268e
MichaelWiss <michael.wiss@gmail.com>
2018-02-28 15:48:57 -0600

    webpack.client

---
+++ webpack.client.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+
+module.exports = {
+   
+   //tell webpack the root file of our server app
+   entry: './src/client/client.js',
+   //tell webpack where to put the output file
+   output: {
+   filename: 'bundle.js',	
+   path: path.resolve(__dirname, 'build')
+   },
+
+   //tell webpack to run babel on every file
+   module: {
+   	rules: [
+   	{
+      test: /\.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+      	presets: [
+      	'react',
+      	'stage-0',
+        ['env', { targets: { browsers: ['last 2 versions'] }}] 
+      	]
+      }
+   	 }
+   	]
+   }
+};
