c126571264dc29d0ac441991b528b12ee538e728
MichaelWiss <michael.wiss@gmail.com>
2018-03-03 16:46:00 -0600

    hydrate

--- src/client/client.js
+++ src/client/client.js
@@ -3,4 +3,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Home from './components/Home';
 
-ReactDom.render(<Home />, document.querySelector('#root'));
+ReactDom.hydrate(<Home />, document.querySelector('#root'));
