4468b57c550d67b119b2b2efdf0f355cd1d2afb1
MichaelWiss <michael.wiss@gmail.com>
2018-03-03 17:03:41 -0600

    merge

--- webpack.client.js
+++ webpack.client.js
@@ -10,23 +10,5 @@ module.exports = {
    output: {
    filename: 'bundle.js',	
    path: path.resolve(__dirname, 'public')
-   },
-
-   //tell webpack to run babel on every file
-   module: {
-   	rules: [
-   	{
-      test: /\.js?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      options: {
-      	presets: [
-      	'react',
-      	'stage-0',
-        ['env', { targets: { browsers: ['last 2 versions'] }}] 
-      	]
-      }
-   	 }
-   	]
    }
 };
