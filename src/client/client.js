6e638d317eabd517efb791c80c6632f6585b0e47
MichaelWiss <michael.wiss@gmail.com>
2018-03-04 15:31:46 -0600

    goutes

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 //startup point for client side app
 import React from 'react';
 import ReactDom from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDom.hydrate(<Home />, document.querySelector('#root'));
+ReactDom.hydrate(
+	<BrowserRouter>
+	 <Routes />
+	</BrowserRouter> 
+	, document.querySelector('#root')
+);
