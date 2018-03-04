b770151a790b09936bb155a9aaa2f66b137101bd
MichaelWiss <michael.wiss@gmail.com>
2018-03-04 17:32:40 -0600

    :renderer

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { renderToString } from 'react-dom-server';
-import Home from '../client/components/Home';
+import { StaticRouter } from 'react-router-dom';
 
 export default () => {
      const content = renderToString(<Home />);
