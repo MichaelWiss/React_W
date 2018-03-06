83bd935a15b1eb0fe214198c3fd61e61061a5722
MichaelWiss <michael.wiss@gmail.com>
2018-03-05 20:32:35 -0600

    store in index,js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 
 const app = express();
@@ -7,7 +8,11 @@ const app = express();
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
-   res.send(renderer(req));
+   const store = createStore();
+
+   //some logic ti initialize and load data into store
+
+   res.send(renderer(req, store));
 });
 
 
