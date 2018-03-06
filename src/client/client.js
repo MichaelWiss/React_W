46e5c4541628fb3eae4bbfa7ee963b2823ba839a
MichaelWiss <michael.wiss@gmail.com>
2018-03-05 20:00:02 -0600

    thunk, provider

--- src/client/client.js
+++ src/client/client.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import Routes from './Routes';
 
 ReactDom.hydrate(
